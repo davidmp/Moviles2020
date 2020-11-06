524ef47d0d679f206dc0ec8e1347179ca94e73f7
davidmp <mamanipari@gmail.com>
2020-11-06 08:21:37 -0500

    QR Flutter Scaner

--- upeu_qr/lib/ui/home.dart
+++ upeu_qr/lib/ui/home.dart
@@ -10,14 +10,52 @@ class Home extends StatefulWidget{
   }
 }
 
-
 class HomeState extends State<Home>{
+  String resultado="Contenido de QR leer ";
+
+  Future scanQR() async{
+    try{
+      String qrResult= await BarcodeScanner.scan();
+      setState(() {
+        resultado=qrResult;
+      });
+    } on PlatformException catch(ex){
+      if(ex.code==BarcodeScanner.CameraAccessDenied){
+        setState(() {
+          resultado="Tiene permiso denegado";
+        });
+      }else{
+        setState(() {
+          resultado="Tiene un error desconocido $ex";
+        });
+      }
+    } on FormatException{
+      setState(() {
+        resultado="Intentar Nuevamente";
+      });
+    }catch(e){
+      resultado="Error desconocido $e";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UpeU Qr"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(resultado),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: scanQR,
+          backgroundColor: Colors.teal,
+          icon: Icon(Icons.camera_alt_outlined),
+          label: Text("QR scan")
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
-
 
 }
