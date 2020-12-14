90960a6b3f2cddbca7aca1149b4eb63fea490667
davidmp <mamanipari@gmail.com>
2020-12-13 22:34:21 -0500

    Personalizar toolbar

--- UpeuActivity/app/src/main/java/pe/edu/upeu/upeuactivity/TabxActivity.kt
+++ UpeuActivity/app/src/main/java/pe/edu/upeu/upeuactivity/TabxActivity.kt
@@ -1,11 +1,13 @@
 package pe.edu.upeu.upeuactivity
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.graphics.PorterDuff
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_tabx.*
 import pe.edu.upeu.upeuactivity.ui.tabs.PageAdapter
 
@@ -16,8 +18,33 @@ class TabxActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tabx)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true).apply {
+            title ="DMP"
+            }
+
+        val backArrow = resources.getDrawable(android.R.drawable.ic_menu_camera)
+        backArrow.setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP)
+        supportActionBar?.setHomeAsUpIndicator(backArrow)
         supportActionBar?.setHomeButtonEnabled(true)
+        val colores = intArrayOf(-0xdaa887, -0xc18b6e, -0x593f33)
+        val g = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colores)
+        supportActionBar?.setBackgroundDrawable(g)
+       /*supportActionBar?.setBackgroundDrawable(
+            GradientDrawable().apply {
+
+                colors = intArrayOf(
+                    //Color.parseColor("#FF6347"),
+                    Color.parseColor("#DEAA88"),
+                    Color.parseColor("#EEE600")
+                )
+                gradientType = GradientDrawable.LINEAR_GRADIENT
+
+                shape = GradientDrawable.LINE
+                orientation = GradientDrawable.Orientation.LEFT_RIGHT
+                setStroke(2, Color.parseColor("#CD5700"))
+            }
+        )*/
+
 
         viewPager.adapter=PageAdapter(supportFragmentManager)
         tabLayout.setupWithViewPager(viewPager)
@@ -27,6 +54,11 @@ class TabxActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun setActionBar(toolbar: Toolbar?) {
+
+        super.setActionBar(toolbar)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
